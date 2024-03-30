9bdd990e4452d7d8fd7d9fa2ec084ee680b1a844
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2024-03-30 20:19:37 +0500

    latest

--- ClubMembershipApplication/obj/Debug/net8.0/ClubMembershipApplication.AssemblyInfo.cs
+++ ClubMembershipApplication/obj/Debug/net8.0/ClubMembershipApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubMembershipApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5a1c4b0db9ef020f36bc10511231c8ed6023a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e40d39ae53300b3a2e6dd370a72a20397b7fc35")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubMembershipApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubMembershipApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
